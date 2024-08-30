195c70cdf2255a0154d468a204328d9b4686a1b8
Mohit <144538075+Mohit-Alive@users.noreply.github.com>
2024-08-30 10:46:51 +0530

    Create 01-List-Operations.py

---
+++ 04-List/01-List-Operations.py
@@ -0,0 +1,27 @@
+list1 = [10, 30, 1, 2, 3, 2, 4, 5]
+# Add
+list1.append(7)
+list.insert(1,3)
+
+# Sort
+list1.sort()
+
+# Delete
+list1.remove(30)
+
+# Access
+print(list1[3])
+
+# Slicing
+print(list1[2:6])
+
+# Reverse
+print(list1[::-1])
+
+# Occurence
+print(list1.count(2))
+
+# Length
+print(len(list1))
+
+print(list1)
