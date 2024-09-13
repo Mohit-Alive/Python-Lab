60347eab002fed7fed8769b4679ab09781834e93
Mohit <144538075+Mohit-Alive@users.noreply.github.com>
2024-09-13 10:46:40 +0530

    Create Tuple.py

---
+++ 05-Tuple amd Sets/Tuple.py
@@ -0,0 +1,46 @@
+tuple1 = ()
+
+tuple1 = tuple1 + (1, 2, 4, 7, 10)
+
+# Sorting
+list(tuple1).sort()
+print(tuple1)
+
+# Accesssing
+print(tuple1[1])
+
+# Slicing
+print(tuple1[1:2])
+
+# Length
+print(len(tuple1))
+
+# Maximum and minimum
+print(max(tuple1))
+print(min(tuple1))
+
+# Type
+print(type(tuple1))
+print(isinstance(tuple1, tuple))
+
+# Delete
+del  tuple1
+
+# Sets
+sets1 = {1,4,7,9,10}
+
+# Adding
+sets1.add(5)
+print(sets1)
+
+# Remove
+sets1.remove(1)
+print(sets1)
+
+# Remove from last
+sets1.pop()
+print(sets1)
+
+# Clear the set
+sets1.clear()
+print(sets1)
